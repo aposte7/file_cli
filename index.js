c973a385d477cb57d8babbfb1d2914c1fe954638
aposte7 <olman4jesus@gmail.com>
2025-09-08 21:17:08 +0300

    initialize project with index.js and package.json

---
+++ index.js
@@ -0,0 +1,5 @@
+import fsPromises from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
